2cf6e5665431a9b1e6d995397fbc9f6bd5076f4a
Keiichi Lindley <digizn@gmail.com>
2020-09-28 05:40:13 -0600

    Add warning and public and private urls

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const os = require("os");
 const npm = require("npm");
 const ngrok = require("ngrok");
 const connectToMe = require("./src/connectToMe");
-const QRIFY = require("./src/qrify");
+const colors = require("colors");
+const qrify = require("./src/qrify");
 
 let spawn = require("child_process").spawn;
 
@@ -13,23 +14,29 @@ const isWindows = async () => {
   return os.type().indexOf("Win") > -1;
 };
 
-const connect = async () => {
+const start = async () => {
+  let url = await connectToMe()
   let win = await isWindows();
   if (win) {
     cmd = "npx.cmd";
   }
   let script = spawn(cmd, ["http-server"]);
   script.stdout.on("data", (data) => {
-    console.log(data.slice(0, data.length - 1).toString("utf8"));
+    console.log(data.slice(0, data.length - 1).toString("utf8").bold);
   });
-  connectToMe().then((url) => {
-    console.log(new QRIFY(url).ascii);
-  });
+  console.log("\n          <!>::WARNING::<!>        \n".bold.red);
+  console.log("  YOU ARE NOW PUBLICY ACCESSIBLE     ".bold.white.bgRed);
+  console.log(`     ${url}   `.bold.white.bgRed);
+  console.log(`        http://127.0.0.1:4040        `.bold.white.bgRed);
+  console.log(new qrify(url).ascii.bold.blue);
+
   process.on("SIGINT", function () {
-    script.stdin.pause();
     script.kill();
     ngrok.kill();
   });
 };
 
-connect();
+start();
+
+
+
