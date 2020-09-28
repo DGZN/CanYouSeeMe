cb0513898bf353a92eac49131d5669ecd54e46df
Keiichi Lindley <digizn@gmail.com>
2020-09-28 04:13:46 -0600

    refactor

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
-#!/usr/bin/env node
+const os = require("os");
 const npm = require('npm')
 const ngrok = require('ngrok')
 const connectToMe = require('./src/connectToMe');
 const QRIFY = require('./src/qrify');
 
-npm.load(() => {
-  npm.run('serve');
-  connectToMe().then((url) => {
-    console.log(new QRIFY(url).ascii)
-  });
-})
+let spawn = require("child_process").spawn;
 
-process.on("SIGINT", function () {
-  ngrok.kill();
-});
+let cmd = "npx";
+
+const isWindows = async () => {
+  return os.type().indexOf('Win') > -1;
+}
+
+const connect = async () => {
+  let win = await isWindows();
+  if (win) {
+    cmd = "npx.cmd";
+  }
+  let script = spawn(cmd, ["http-server"]);
+  script.stdout.on("data", (data) => {
+    console.log(data.slice(0, data.length - 1).toString("utf8"));
+  });
+  connectToMe().then((url) => {
+    console.log(new QRIFY(url).ascii);
+  });
+  process.on("SIGINT", function () {
+    script.stdin.pause();
+    script.kill();
+    ngrok.kill();
+  });
+}
+
+connect()
+
